764836fc406b8cc449a06928ae55ef758edeab50
Supawee Bom Ippoodom <i.supawee@gmail.com>
2025-06-26 00:15:15 +0700

    Fix navigation bar

--- laravel/resources/views/layout/components/navigation.blade.php
+++ laravel/resources/views/layout/components/navigation.blade.php
@@ -140,7 +140,7 @@
         <!-- navbar -->
         <nav class="navbar navbar-area navbar-expand-lg nav-style-01">
             <div class="container nav-container d-flex flex-wrap justify-content-around align-items-center my-0 px-0 h-100">
-                <div class="responsive-mobile-menu col-12 col-md-3 col-lg-2 h-100 align-items-center">
+                <div class="responsive-mobile-menu col-12 col-md-3 h-100 align-items-center pr-5">
                     <div class="logo-wrapper">
                         <a href="{{ route('home.index') }}" class="logo">
                             <img src="{{ asset('assets/images/logo_h.svg') }}" alt="logo">
