0b8e9e367608c3b5073e571214d7c16462d923c9
Supawee Bom Ippoodom <i.supawee@gmail.com>
2025-08-25 17:41:52 +0700

    Integrate Homepage
    - Navigation section with WebSettings.
    - HeroBanner with Banners
    - Vision & Mission with AboutusContent
    - Research section with Researches

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -4,9 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Banners;
+use App\Models\AboutusContents;
+use App\Models\Researches;
+
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+
+        $banners = Banners::with(['hotspots' => fn ($q) => $q->Published()])->Published()->get();
+        $aboutusContent = AboutusContents::Published()->first();
+        $highlightResearch = Researches::Highlighted()->first();
+        // $productReview = ProductReviews::Highlighted()->get()->toArray();
+        // $reviews = Reviews::Published()->get();
+        // dd( $highlightResearch );
+
+        $dataCompact = [
+            'banners',
+            'aboutusContent',
+            'highlightResearch'
+        ];
+
+        return view('home.index', compact( $dataCompact ));
     }
 }
