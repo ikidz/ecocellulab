4a2d897c23ffb5966355a4f8581427beede159d6
Supawee Bom Ippoodom <i.supawee@gmail.com>
2025-09-01 04:16:15 +0700

    Fix NovaServiceProvider

--- laravel/app/Providers/NovaServiceProvider.php
+++ laravel/app/Providers/NovaServiceProvider.php
@@ -98,7 +98,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'i.supawee@gmail.com',
             ]);
         });
     }
