87d4abacf8f12768ef8a665e4965f206a1a56b62
Supawee Bom Ippoodom <i.supawee@gmail.com>
2025-09-04 02:16:07 +0700

    Update stylesheet

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -30,7 +30,7 @@
     @if (app()->environment('local'))
         <script type="module" src="http://localhost:5173/resources/sass/style.scss"></script>
     @else
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=1.4') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=1.5') }}">
     @endif
     <!-- responsive Stylesheet -->
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css?v=1.3') }}">
